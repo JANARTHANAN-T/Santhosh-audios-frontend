be9e8cae00f0b2e7861bcda384384cf4cb2f52fd
JANARTHANAN-T <janathangavel0911@gmail.com>
2023-04-15 10:28:39 +0530

    gallery fetch data

--- src/pages/GalleryPage/GalleryPage.jsx
+++ src/pages/GalleryPage/GalleryPage.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import TopNavBtn from '../../components/TopNavBtn/TopNavBtn'
+import axios from 'axios'
 function GalleryPage() {
+
+  var data=fetch('https://script.google.com/macros/s/AKfycbx8LmqtHUIkf__2hQBkn-3LDZCjbudbymogs4unYAcUmucTmKq1BH1K2yGMOVojaLx-/exec')
+    data.then((value)=>{
+      console.log(value);
+    }).catch((err)=>{
+      console.log(err);
+    })
+
   return (
     <div>
     <Navbar floatingNav />
@@ -13,6 +22,7 @@ function GalleryPage() {
         <button className='rounded-full border-2 border-[#312e81] px-4 py-2 hover:bg-[#312e81] text-[#312e81] font-semibold hover:text-white md:m-6 m-2'>Van/Bus Lighting</button>
         <button className='rounded-full border-2 border-[#312e81] px-4 py-2 hover:bg-[#312e81] text-[#312e81] font-semibold hover:text-white md:m-6 m-2'>Other Service</button>
     </div>
+
     <TopNavBtn />
     </div>
   )
